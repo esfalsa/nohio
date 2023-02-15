62883a44cfced83fdb69729f7ce56126507004f2
esfalsa <pronoun.nationstates@gmail.com>
2023-02-15 00:38:23 -0800

    feat: censor DOM mutations

--- src/content.ts
+++ src/content.ts
@@ -1,32 +1,52 @@
 import browser from "webextension-polyfill";
 import { type ExtensionMessage } from "./types";
 
-const walker = document.createTreeWalker(
-  document.body,
-  NodeFilter.SHOW_TEXT,
-  (node) =>
+const ohioRegex = /ohio/gi;
+
+function censor(node: Node) {
+  const walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, (node) =>
     node.textContent ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT
-);
-
-let replaced = 0;
-
-while (walker.nextNode()) {
-  walker.currentNode.textContent &&= walker.currentNode.textContent.replace(
-    /ohio/gi,
-    () => {
-      replaced++;
-      return "****";
-    }
   );
+
+  let replaced = 0;
+
+  while (walker.nextNode()) {
+    walker.currentNode.textContent &&= walker.currentNode.textContent.replace(
+      ohioRegex,
+      () => {
+        replaced++;
+        return "****";
+      }
+    );
+  }
+
+  if (replaced > 0) {
+    browser.runtime.sendMessage({
+      type: "setBadgeText",
+      value: replaced.toString(),
+    } satisfies ExtensionMessage);
+  }
+
+  browser.storage.local.get("blocked").then(({ blocked }) => {
+    browser.storage.local.set({ blocked: (blocked || 0) + replaced });
+  });
 }
 
-if (replaced > 0) {
-  browser.runtime.sendMessage({
-    type: "setBadgeText",
-    value: replaced.toString(),
-  } satisfies ExtensionMessage);
-}
+censor(document);
 
-browser.storage.local.get("blocked").then(({ blocked }) => {
-  browser.storage.local.set({ blocked: (blocked || 0) + replaced });
+const observer = new MutationObserver((mutationList) => {
+  mutationList
+    .flatMap(({ addedNodes }) =>
+      [...addedNodes].filter(
+        ({ textContent }) => textContent && ohioRegex.test(textContent)
+      )
+    )
+    .forEach((node) => {
+      censor(node);
+    });
+});
+
+observer.observe(document, {
+  subtree: true,
+  childList: true,
 });
